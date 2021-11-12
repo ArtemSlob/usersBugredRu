eaffda87c74b4690391c718b6d3c51d41d3132b4
surface\artem <artem.slobodianiuk@gmail.com>
2021-11-12 13:20:54 +0200

    API Test Project Created

---
+++ usersBugredRu/usersBugredRu/APITests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace usersBugredRu
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
