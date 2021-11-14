4155f51a3932f6de21644c7fafdf32a4ee0c734d
surface\artem <artem.slobodianiuk@gmail.com>
2021-11-14 16:36:37 +0200

    no message

--- usersBugredRu/usersBugredRu/APITests.cs
+++ usersBugredRu/usersBugredRu/APITests.cs
@@ -1,4 +1,9 @@
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace usersBugredRu
 {
@@ -9,10 +14,37 @@ namespace usersBugredRu
         {
         }
 
-        [Test]
-        public void Test1()
+        public void RegistrationTest()
         {
-            Assert.Pass();
+            RestClient client = new RestClient("http://users.bugred.ru/tasks/rest/doregister")
+            {
+                Timeout = 300000
+            };
+            RestRequest request = new RestRequest(Method.POST);
+            //request.AddHeader("Content-Type", "application/json");
+
+            Dictionary<string, string> body = GenerateUserData();
+            request.AddJsonBody(body);
+
+            IRestResponse response = client.Execute(request);
+
+            JObject json = JObject.Parse(response.Content);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(body["email"], json["account"]["email"]?.ToString());
+        }
+
+        public Dictionary<string, string> GenerateUserData()
+        {
+            string email = "milli1@gmail.ru";
+            string name = "Mashenka";   
+            string password = "1";
+            return new Dictionary<string, string>()
+            {
+                {"email", email },
+                {"name", name },
+                {"password", password }
+            };
         }
     }
 }
